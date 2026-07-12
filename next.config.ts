873eeed380bac66b8c093ba84647e79519a3efbf
hasan sbeity <th.rider.clan@gmail.com>
2026-07-13 01:59:56 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       {
         protocol: "https",
